d3812752c1680c76ff871d13d49ddf6dfafae008
matiasvinjevoll <matias.vinjevoll@nav.no>
2020-04-03 16:37:27 +0200

    FO-2833 legg til skjema

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.less';
 import Banner from './components/banner/Banner';
 import { visitMetrikk } from './components/util/frontendlogger';
+import Skjema from "./components/skjema/Skjema";
 
 function App() {
     visitMetrikk();
     return (
         <div className="app">
             <Banner />
+            <Skjema />
         </div>
     );
 }
